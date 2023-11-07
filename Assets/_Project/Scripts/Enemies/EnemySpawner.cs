15d72f1a515bec1b5dc02a9378ae815064daaefc
victor <victor.plasencia@rovio.com>
2023-11-07 21:59:32 +0000

    update

--- Assets/_Project/Scripts/Enemies/EnemySpawner.cs
+++ Assets/_Project/Scripts/Enemies/EnemySpawner.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] GameObject enemy;
-    int enemyPoolSize = 20;
+    readonly int enemyPoolSize = 20;
     Queue<GameObject> enemyQueue = new();
     GameObject inUseEnemy;
     float speed = 1f;
 
     float cameraHalfHeight;
     float spawnInterval = 2f;
-    
+
 
     void Start()
     {
@@ -27,13 +27,18 @@ public class EnemySpawner : MonoBehaviour
     }
     private GameObject GetEnemy()
     {
-        if(enemyQueue.Count == 0)
+        if (enemyQueue.Count == 0)
         {
-
+            enemyQueue.Enqueue(enemy);
+            return enemy;
         }
-        GameObject pooledEnemy = enemyQueue.Dequeue();
-        pooledEnemy.SetActive(true);
-        return pooledEnemy;
+        else
+        {
+            GameObject pooledEnemy = enemyQueue.Dequeue();
+            pooledEnemy.SetActive(true);
+            return pooledEnemy;
+        }
+
     }
 
     IEnumerator Spawner()
@@ -43,7 +48,7 @@ public class EnemySpawner : MonoBehaviour
             Vector2 spawnPos = GetRandomPos();
             Vector2 direction = (Vector2.zero - spawnPos).normalized; //Calculate direction to center
             Vector2 toCenter = Vector2.MoveTowards(spawnPos, Vector2.zero, speed * Time.deltaTime);
-            
+
             inUseEnemy = GetEnemy();
             inUseEnemy.transform.position = spawnPos;
             inUseEnemy.GetComponent<Rigidbody2D>().velocity = toCenter;
@@ -51,14 +56,14 @@ public class EnemySpawner : MonoBehaviour
             yield return new WaitForSeconds(spawnInterval);
         }
 
-        
+
     }
 
     private Vector2 GetRandomPos()
     {
-        int side = Random.Range(0,4);
+        int side = Random.Range(0, 4);
 
-        switch(side)
+        switch (side)
         {
             //top
             case 0: return new Vector2(Random.Range(-ScreenBounds.bounds.x, ScreenBounds.bounds.x), cameraHalfHeight);
