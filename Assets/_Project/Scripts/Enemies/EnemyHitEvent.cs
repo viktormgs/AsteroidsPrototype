6efecf4095e6fbac3c1b2e50b0af2e8130b0baca
victor <victor.plasencia@rovio.com>
2023-11-26 00:16:30 +0000

    todoupdate

--- Assets/_Project/Scripts/Enemies/EnemyHitEvent.cs
+++ Assets/_Project/Scripts/Enemies/EnemyHitEvent.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class EnemyHit : MonoBehaviour
+public class EnemyHitEvent : MonoBehaviour
 {
     public static Action EnemyInteraction;
 
